8c3b4cbb51aef11dd277e274ea08024926adf7e1
kimchijinju <hanbimkim@gmail.com>
2023-01-11 22:04:59 +0900

    cpp00-ex00

---
+++ cpp_module/00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void inputToUpper(char *string)
+{
+	for (int i = 0; string[i]; ++i)
+		string[i] = std::toupper(string[i]);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+	for (int i = 1; i < argc; ++i)
+	{
+		inputToUpper(argv[i]);
+		std::cout << argv[i];
+	}
+	std::cout << '\n';
+}
+
+
