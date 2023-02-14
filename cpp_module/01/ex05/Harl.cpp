a01ea13e57f50b6d118a641cfbedcf75eb76b692
kimchijinju <hanbimkim@gmail.com>
2023-02-14 17:31:56 +0900

    fix: format

--- cpp_module/01/ex05/Harl.cpp
+++ cpp_module/01/ex05/Harl.cpp
@@ -19,21 +19,21 @@ void Harl::complain(std::string level) {
 }
 
 void Harl::debug() {
-  std::cout
-      << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!\n";
+  std::cout << "[ DEBUG ]\n"
+            << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!\n";
 }
 
 void Harl::info() {
-  std::cout
-      << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+  std::cout << "[ INFO ]\n"
+            << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 
 void Harl::warning() {
-  std::cout
-      << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
+  std::cout << "[ WARNING ]\n"
+            << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
 }
 
 void Harl::error() {
-  std::cout
-      << "This is unacceptable! I want to speak to the manager now.\n";
+  std::cout << "[ ERROR ]\n"
+            << "This is unacceptable! I want to speak to the manager now.\n";
 }
