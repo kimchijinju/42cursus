d3b6a08b7c068b1d6dad9d05d4b44feebf1648c8
kimchijinju <hanbimkim@gmail.com>
2023-02-15 14:55:16 +0900

    fix: set name argument

--- cpp_module/01/ex01/ZombieHorder.cpp
+++ cpp_module/01/ex01/ZombieHorder.cpp
@@ -4,7 +4,7 @@ Zombie *zombieHorde(int N, std::string name) {
     Zombie *zombie = new Zombie[N];
 
     for (int i = 0; i < N; ++i) {
-      zombie[i].setName(name + std::to_string(i));
+      zombie[i].setName(name);
     }
     return zombie;
 }
