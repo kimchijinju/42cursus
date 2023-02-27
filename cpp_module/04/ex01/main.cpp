9db6cfc1db5534b3dbd15b911d9d44f74bc6ec2d
kimchijinju <hanbimkim@gmail.com>
2023-02-27 16:26:12 +0900

    feat: ex02

--- cpp_module/04/ex01/main.cpp
+++ cpp_module/04/ex01/main.cpp
@@ -4,11 +4,28 @@
 #include "Dog.hpp"
 
 int main() {
-  Animal *dog = new Dog[3];
-  Animal *cat = new Cat[3];
+  Animal *animal[4];
 
-  dog[0].getType();
-  cat[0].getType();
-  system("leaks Brain");
+  for (int i = 0; i < 4; ++i) {
+    if (i == 0 || i == 1)
+      animal[i] = new Cat();
+    else
+      animal[i] = new Dog();
+  }
+
+  for (int i = 0; i < 4; ++i) {
+    animal[i]->makeSound();
+  }
+
+  (*animal)[0] = (*animal)[3]; // deep copy
+  std::cout << "0 : " << animal[0]->getType() << "\n1 : " << animal[3]->getType() << '\n';
+
+  for (int i = 0; i < 4; ++i) {
+    animal[i]->makeSound();
+  }
+
+  for (int i = 0; i < 4; ++i) {
+    delete animal[i];
+  }
   return 0;
 }
