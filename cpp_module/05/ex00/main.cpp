f6e580bcc221907649187a84be7bff6040073555
kimchijinju <hanbimkim@gmail.com>
2023-03-01 20:27:55 +0900

    feat: ex01

---
+++ cpp_module/05/ex00/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+int main() {
+  try {
+    Bureaucrat hanbkim = Bureaucrat("hanbkim", 5);
+    std::cout << hanbkim << '\n';
+
+//    for (int i = 0; i < 5; ++i) {
+//      hanbkim.increaseGrade();
+//      std::cout << hanbkim;
+//    }
+
+    Bureaucrat joushin = Bureaucrat("joushin", 0);
+    std::cout << joushin;
+
+  } catch (std::exception &e) {
+    std::cout << e.what();
+  }
+
+}
