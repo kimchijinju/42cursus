72b8b0f4e47640363c0338299e31646954f63d57
kimchijinju <hanbimkim@gmail.com>
2023-03-20 15:33:24 +0900

    feat: ex01 const template

--- cpp_module/07/ex01/iter.hpp
+++ cpp_module/07/ex01/iter.hpp
@@ -8,6 +8,13 @@
 #include <cstdlib>
 
 template<typename T>
+void iter(T *begin, size_t length, void (*f)(const T&)) {
+  for (size_t i = 0; i < length; ++i) {
+    f(begin[i]);
+  }
+}
+
+template<typename T>
 void iter(T *begin, size_t length, void (*f)(T&)) {
   for (size_t i = 0; i < length; ++i) {
     f(begin[i]);
